198f934fe31dd0617aa6ab0e6249ddc15e815a52
Adilson Oliveira <adilsonoj@yahoo.com.br>
2021-02-12 17:32:19 -0300

    add retorno dos objetos relacionados

--- conta/serializer.py
+++ conta/serializer.py
@@ -8,12 +8,16 @@ class AccountSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ClientSerializer(serializers.ModelSerializer):
+    account = AccountSerializer(read_only=True)
+    
     class Meta:
         model = Client
         fields = '__all__'
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    client = ClientSerializer(read_only=True)
+
     class Meta:
         model = Transaction
         fields = '__all__'
